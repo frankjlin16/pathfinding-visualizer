38a7067228b566659ede1e2a1e19648a9222197c
Alek Broman <alek@polymods.com>
2021-03-16 12:36:11 -0500

    Update node.jsx

--- src/pathfinder/node.jsx
+++ src/pathfinder/node.jsx
@@ -5,7 +5,7 @@ import "./node.css";
 class Node extends Component {
 	render() {
 		const { row, column, isStart, isFinish, isWall, onMouseDown, onMouseEnter, onMouseUp } = this.props;
-		const type = isStart ? " node-start" : isFinish ? " node-finish" : isWall ? " node-wall" : "";
+		const type = isStart ? " start" : isFinish ? " finish" : isWall ? " wall" : "";
 		return (
 			<div
 				id={`node-${row}-${column}`}
