4812a040f9b6187c26b1c64b2125f5cf648b6ad4
Alek Broman <alek@polymods.com>
2021-03-30 13:29:34 -0500

    Updates

---
+++ precache-manifest.9f7eb704695a06221924032fcb929224.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2857f4bd3afb54990544b5361c029c",
+    "url": "/pathfinding-visualizer/index.html"
+  },
+  {
+    "revision": "debefedf2b98888263b7",
+    "url": "/pathfinding-visualizer/static/css/2.735da23c.chunk.css"
+  },
+  {
+    "revision": "c01d17e0d0a2adaa2fb0",
+    "url": "/pathfinding-visualizer/static/css/main.c63054b7.chunk.css"
+  },
+  {
+    "revision": "debefedf2b98888263b7",
+    "url": "/pathfinding-visualizer/static/js/2.defd1e3d.chunk.js"
+  },
+  {
+    "revision": "61999b26f20866c76d81d465d362b3a4",
+    "url": "/pathfinding-visualizer/static/js/2.defd1e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01d17e0d0a2adaa2fb0",
+    "url": "/pathfinding-visualizer/static/js/main.c5a163b3.chunk.js"
+  },
+  {
+    "revision": "a2b7021554e2339a14f1",
+    "url": "/pathfinding-visualizer/static/js/runtime-main.b6ae0412.js"
+  },
+  {
+    "revision": "5b0723ee967e9551b9e35d6968869099",
+    "url": "/pathfinding-visualizer/static/media/svg-decorator-blob-6.5b0723ee.svg"
+  },
+  {
+    "revision": "c86fb21a2396dc9a8149429f1cd451cf",
+    "url": "/pathfinding-visualizer/static/media/svg-decorator-blob-7.c86fb21a.svg"
+  },
+  {
+    "revision": "06b7e9297b931c8f3c729d9666faf31f",
+    "url": "/pathfinding-visualizer/static/media/svg-decorator-blob-9.06b7e929.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2749aae912fcdc03d6ae669c5ea7c2b.js"
+  "/pathfinding-visualizer/precache-manifest.9f7eb704695a06221924032fcb929224.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pathfinding-visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
