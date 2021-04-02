328a7ca7701bc7e63c8c2270da25c38ee8652192
Alek Broman <alek@polymods.com>
2021-04-02 01:23:24 -0500

    Update Header.js

--- src/pathfinder/Header.js
+++ src/pathfinder/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "pathfinder/logo.png";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
@@ -7,30 +8,35 @@ import { SectionHeading, Subheading as SubheadingBase } from "components/misc/He
 import { ReactComponent as SvgDecoratorBlob } from "images/svg-decorator-blob-6.svg";
 
 const HeaderContainer = tw.div`w-full flex flex-col items-center`;
-const Heading = tw(SectionHeading)`w-full`;
+const LogoContainer = tw.div`flex items-center justify-center md:justify-start flex-col md:flex-row`;
+const LogoImg = tw.img`w-16`;
+const Heading = tw(SectionHeading)`w-full ml-0 md:ml-4 mt-4 md:mt-0`;
 const Subheading = tw(SubheadingBase)`mb-4`;
 const Description = tw(SectionDescription)`w-full text-center`;
 const DecoratorBlob = styled(SvgDecoratorBlob)`
-  ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-64 w-64 opacity-25 transform -translate-x-2/3 -translate-y-1/2`}
+	${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-64 w-64 opacity-25 transform -translate-x-2/3 -translate-y-1/2`}
 `;
 
 export default ({
-  heading = "Pathfinding Visualizer",
-  subheading = "by Alek Broman | v.02",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	heading = "Pathfinding Visualizer",
+	subheading = "by Alek Broman | 0.1.1",
+	description = "This is a visualizer for multiple pathfinding algorithms. A pathfinding algorithm seeks to find the shortest path between two points. Continue to the tutorial below to begin visualizing!",
 }) => {
-  return (
-    <Container>
-      <ContentWithPaddingXl>
-        <ContentWithPaddingXl>
-          <HeaderContainer>
-            <Heading>{heading}</Heading>
-            {subheading && <Subheading>{subheading}</Subheading>}
-            {description && <Description>{description}</Description>}
-          </HeaderContainer>
-        </ContentWithPaddingXl>
-      </ContentWithPaddingXl>
-      <DecoratorBlob />
-    </Container>
-  );
+	return (
+		<Container>
+			<ContentWithPaddingXl>
+				<ContentWithPaddingXl>
+					<HeaderContainer>
+						<LogoContainer>
+							<LogoImg src={logo} />
+							<Heading>{heading}</Heading>
+						</LogoContainer>
+						{subheading && <Subheading>{subheading}</Subheading>}
+						{description && <Description>{description}</Description>}
+					</HeaderContainer>
+				</ContentWithPaddingXl>
+			</ContentWithPaddingXl>
+			<DecoratorBlob />
+		</Container>
+	);
 };
