3a258a61bb0ae0a26c43ea013d18c36e20194213
Alek Broman <alek@polymods.com>
2021-04-12 01:46:57 -0500

    Update Node.js

--- src/pathfinder/Node.js
+++ src/pathfinder/Node.js
@@ -4,9 +4,18 @@ import React, { Component } from "react";
 
 class Node extends Component {
 	render() {
-		const { style, row, column, isStart, isFinish, isWall, isVisited, isPath } = this.props;
-		const type = isStart ? "start" : isFinish ? "finish" : isWall ? "wall" : isVisited ? "visited" : isPath ? "path" : "";
-		return <div style={style} id={`node-${row}-${column}`} className={`node ${type}`}></div>;
+		const { style, row, column, isStart, isFinish, isWall, isVisited, isPath, onMouseDown, onMouseEnter, onMouseUp } = this.props;
+		const type = isStart ? " start" : isFinish ? " finish" : isWall ? " wall" : isVisited ? " visited" : isPath ? " path" : "";
+		return (
+			<div
+				style={style}
+				id={`node-${row}-${column}`}
+				className={`node${type}`}
+				onMouseDown={() => onMouseDown(row, column)}
+				onMouseEnter={() => onMouseEnter(row, column)}
+				onMouseUp={() => onMouseUp()}
+			></div>
+		);
 	}
 }
 
