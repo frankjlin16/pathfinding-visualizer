b93ac000717518b00278c85a899178554623aa6f
Alek Broman <alek@polymods.com>
2021-04-15 03:54:50 -0500

    Update breadth-first.js

---
+++ src/pathfinder/algorithms/breadth-first.js
@@ -0,0 +1,38 @@
+export function breadthFirst(grid, startNode, finishNode) {
+	const visitedNodes = [];
+	const unvisitedNodes = [];
+	unvisitedNodes.push(startNode);
+	while (unvisitedNodes.length) {
+		let closestNode = unvisitedNodes.shift();
+		if (!closestNode.isWall) {
+			if (closestNode === finishNode) return visitedNodes;
+			closestNode.isVisited = true;
+			visitedNodes.push(closestNode);
+			const neighbors = getNeighbors(closestNode, grid);
+			for (const neighbor of neighbors) {
+				neighbor.previousNode = closestNode;
+				if (isNeighborVisited(neighbor, unvisitedNodes)) unvisitedNodes.push(neighbor);
+			}
+		}
+	}
+	return visitedNodes;
+}
+
+function getNeighbors(node, grid) {
+	const neighbors = [];
+	const { row, column } = node;
+	if (row !== 0) neighbors.push(grid[row - 1][column]);
+	if (row !== grid.length - 1) neighbors.push(grid[row + 1][column]);
+	if (column !== 0) neighbors.push(grid[row][column - 1]);
+	if (column !== grid[0].length - 1) neighbors.push(grid[row][column + 1]);
+	return neighbors.filter((neighbor) => !neighbor.isVisited);
+}
+
+function isNeighborVisited(neighbor, unvisitedNodes) {
+	for (const node of unvisitedNodes) {
+		if (node.row === neighbor.row && node.column === neighbor.column) {
+			return false;
+		}
+	}
+	return true;
+}
