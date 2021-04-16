fd51591917ebde8c7102aeac51a013eb1834de7f
Alek Broman <alek@polymods.com>
2021-04-15 20:50:32 -0500

    Update Footer.js

--- src/pathfinder/Footer.js
+++ src/pathfinder/Footer.js
@@ -11,6 +11,7 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 const Container = tw(ContainerBase)`bg-gray-900 text-gray-100 -mx-8 -mb-8`;
 const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 const Row = tw.div`flex items-center justify-center flex-col px-8`;
+const Text = tw.p`text-center mt-10 font-medium tracking-wide text-sm text-gray-600`;
 const LogoContainer = tw.div`flex items-center justify-center md:justify-start flex-col sm:flex-row mt-10`;
 const LogoImg = tw.img`w-8`;
 const LogoText = tw.h5`ml-0 sm:ml-2 mt-2 sm:mt-0 text-2xl font-black tracking-wider text-center`;
@@ -21,15 +22,18 @@ const SocialLink = styled.a`
 		${tw`w-5 h-5`}
 	}
 `;
-const CopyrightText = tw.p`text-center mt-10 font-medium tracking-wide text-sm text-gray-600`;
 
 export default () => {
 	return (
 		<Container>
 			<Content>
 				<Row>
-					<CopyrightText css={tw`mt-0`}>1. START/FINISH node transposition not supported on mobile devices.</CopyrightText>
-					<CopyrightText css={tw`mt-0`}>2. Random walk path visualization not supported, only searching.</CopyrightText>
+					<Text css={tw`mt-0`}>
+						1. START/FINISH node transposition not supported on mobile devices.
+					</Text>
+					<Text css={tw`mt-0`}>
+						2. Random walk path visualization not supported, only searching.
+					</Text>
 					<LogoContainer>
 						<LogoImg src={logo} />
 						<LogoText>Pathfinding Visualizer</LogoText>
@@ -45,7 +49,10 @@ export default () => {
 							<FontAwesomeIcon icon={faGithub} />
 						</SocialLink>
 					</SocialLinksContainer>
-					<CopyrightText>2020 – {new Date().getFullYear()} &copy; Alek Broman. All rights reserved.</CopyrightText>
+					<Text>
+						2020 – {new Date().getFullYear()} &copy; Alek Broman. All rights
+						reserved.
+					</Text>
 				</Row>
 			</Content>
 		</Container>
