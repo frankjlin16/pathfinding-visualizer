11471c91cdea5cbfddbd493ddf51202cfbaa88fd
Alek Broman <alek@polymods.com>
2021-04-19 20:35:16 -0500

    Update random-walk.js

--- src/pathfinder/algorithms/random-walk.js
+++ src/pathfinder/algorithms/random-walk.js
@@ -3,10 +3,13 @@ export function randomWalk(grid, startNode, finishNode) {
 	const visitedNodes = [];
 	for (let i = 0; i < 10000; ++i) {
 		if (closestNode === finishNode) return visitedNodes;
-		visitedNodes.push(closestNode);
-		const neighbors = getNeighbors(closestNode, grid);
-		const randomNeighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
-		closestNode = randomNeighbor;
+		if (!closestNode.isWall) {
+			visitedNodes.push(closestNode);
+			const neighbors = getNeighbors(closestNode, grid);
+			const randomNeighbor =
+				neighbors[Math.floor(Math.random() * neighbors.length)];
+			closestNode = randomNeighbor;
+		}
 	}
 	return visitedNodes;
 }
@@ -18,5 +21,5 @@ function getNeighbors(node, grid) {
 	if (row !== grid.length - 1) neighbors.push(grid[row + 1][column]);
 	if (column !== 0) neighbors.push(grid[row][column - 1]);
 	if (column !== grid[0].length - 1) neighbors.push(grid[row][column + 1]);
-	return neighbors.filter((neighbor) => !neighbor.isWall);
+	return neighbors;
 }
