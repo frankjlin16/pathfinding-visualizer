77b65f9b2700fc39702dd1f6f7fc04445af57bbf
Alek Broman <alek@polymods.com>
2021-04-23 16:42:44 -0500

    Revert "Update maze.js"
    This reverts commit f50c4d4cd5b680ba1229b918c9ecd383058aa3f2.

--- src/pathfinder/algorithms/maze.js
+++ src/pathfinder/algorithms/maze.js
@@ -11,37 +11,45 @@ export function maze(grid) {
 
 function range(length) {
 	const result = [];
-	for (let i = 0; i < length; ++i) result.push(i);
+	for (let i = 0; i < length; ++i) {
+		result.push(i);
+	}
 	return result;
 }
 
 function createBorder(grid, rows, columns) {
 	rows.shift();
-	for (const column of columns) walls.push([0, column]);
-	for (const row of rows) {
-		walls.push([row, 0]);
-		walls.push([row, grid[0].length - 1]);
+	for (const node of columns) {
+		walls.push([0, node]);
+	}
+	for (const node of rows) {
+		walls.push([node, 0]);
+		walls.push([node, grid[0].length - 1]);
 	}
 	rows.pop();
 	columns.shift();
 	columns.pop();
-	for (const column of columns) walls.push([grid.length - 1, column]);
+	for (const node of columns) {
+		walls.push([grid.length - 1, node]);
+	}
 }
 
 function createMaze(grid, rows, columns) {
-	if (rows.length < 2 || columns.length < 2) return;
-	let vertical;
+	if (rows.length < 2 || columns.length < 2) {
+		return;
+	}
+	let direction;
 	let number;
 	if (rows.length < columns.length) {
-		vertical = true;
+		direction = 0;
 		number = getOddNumber(columns.length - 1);
-		createWalls(rows, columns, vertical, columns[number]);
+		createWalls(rows, columns, direction, columns[number]);
 		createMaze(grid, rows, columns.slice(0, columns.indexOf(columns[number])));
 		createMaze(grid, rows, columns.slice(columns.indexOf(columns[number]) + 1));
 	} else {
-		vertical = false;
+		direction = 1;
 		number = getOddNumber(rows.length - 1);
-		createWalls(rows, columns, vertical, rows[number]);
+		createWalls(rows, columns, direction, rows[number]);
 		createMaze(grid, rows.slice(0, rows.indexOf(rows[number])), columns);
 		createMaze(grid, rows.slice(rows.indexOf(rows[number]) + 1), columns);
 	}
@@ -50,25 +58,40 @@ function createMaze(grid, rows, columns) {
 function getOddNumber(maximum) {
 	let number = Math.floor(Math.random() * maximum);
 	if (number % 2 === 0) {
-		if (number === maximum) number -= 1;
-		else number += 1;
+		if (number === maximum) {
+			number -= 1;
+		} else {
+			number += 1;
+		}
 	}
 	return number;
 }
 
-function createWalls(rows, columns, vertical, number) {
+function createWalls(rows, columns, direction, number) {
 	const nodes = [];
-	if (vertical) for (const row of rows) nodes.push([row, number]);
-	else for (const column of columns) nodes.push([number, column]);
+	if (direction === 0) {
+		for (const node of rows) {
+			nodes.push([node, number]);
+		}
+	} else {
+		for (const node of columns) {
+			nodes.push([number, node]);
+		}
+	}
 	nodes.splice(getEvenNumber(nodes.length - 1), 1);
-	for (const node of nodes) walls.push(node);
+	for (const node of nodes) {
+		walls.push(node);
+	}
 }
 
 function getEvenNumber(maximum) {
 	let number = Math.floor(Math.random() * maximum);
 	if (number % 2 !== 0) {
-		if (number === maximum) number -= 1;
-		else number += 1;
+		if (number === maximum) {
+			number -= 1;
+		} else {
+			number += 1;
+		}
 	}
 	return number;
 }
